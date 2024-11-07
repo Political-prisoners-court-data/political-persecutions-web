915143b249c2e24ff00b2a05d0ce84b9bbba3073
A. Ivanov <aleksandr.pv.ivanov@gmail.com>
2024-11-08 01:09:30 +0200

    Drizzle removal

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,6 @@ const config = {
   },
   "plugins": [
     "@typescript-eslint",
-    "drizzle"
   ],
   "extends": [
     "next/core-web-vitals",
@@ -40,24 +39,6 @@ const config = {
         }
       }
     ],
-    "drizzle/enforce-delete-with-where": [
-      "error",
-      {
-        "drizzleObjectName": [
-          "db",
-          "ctx.db"
-        ]
-      }
-    ],
-    "drizzle/enforce-update-with-where": [
-      "error",
-      {
-        "drizzleObjectName": [
-          "db",
-          "ctx.db"
-        ]
-      }
-    ]
   }
 }
 module.exports = config;

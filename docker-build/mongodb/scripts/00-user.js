e9a0f168ce49132d2a939c4f822e229804ad1310
A. Ivanov <aleksandr.pv.ivanov@gmail.com>
2024-11-22 00:58:08 +0200

    Dockerized database and web app

---
+++ docker-build/mongodb/scripts/00-user.js
@@ -0,0 +1,15 @@
+//@ts-nocheck
+db = connect( 'mongodb://root:mainpass@localhost:27017/admin' );
+
+db.createUser(
+    {
+        user: 'app',
+        pwd: 'app',
+        roles: [
+            {
+                role: 'dbOwner',
+                db: 'eventsDb'
+            } ]
+} );
+
+printjson( db.getUsers() );
